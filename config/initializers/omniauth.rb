c53e97218130c4153a2be18cf83562f25938229f
Jai Rajput <loveyhtu21@gmail.com>
2016-01-05 13:31:42 +0530

    make it better

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, "1076454195738532", "30514e857c564b5bb155666ee7572095",
            scope: 'public_profile', display: 'page', image_size: 'square'
 
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal'
